a33255b290956f3d85fb76998a034414c3f363d3
Olivia Cheng <olivia@heyitsolivia.com>
2015-12-18 12:48:21 -0500

    Add scrolling and navigation to values page

--- themes/isl/source/js/app.js
+++ themes/isl/source/js/app.js
@@ -2,6 +2,24 @@
 
 (function () {
   'use strict';
-  console.log('Hello, world!');
-})();
+  
+  var $values = $('.value');
+  var $slideNavs = $('.sidenav > ul > li');
+  var offset = $('.value').height() / 3;
 
+  for(var i = 0; i < $values.length; i++) {
+    var slide = $('.js-value-' + (i + 1));
+    var max = slide.offset().top + (slide.height());
+
+      slide.scrollspy({
+        min: i === 0 ? 0 : slide.offset().top - offset,
+        max: max,
+        onEnter: function(element) {
+          var slideNumber = $(element).attr('class').split(' ')[0].split('-')[2];
+          $slideNavs.removeClass('is-active');
+          $('.js-nav-slide-' + slideNumber).addClass('is-active');
+      }
+    });
+  }
+
+})();
